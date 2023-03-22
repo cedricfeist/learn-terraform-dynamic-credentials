40342c614b7894477f4141a6f2f18a5128fee71a
Cedric Feist <cedric.feist@gmail.com>
2023-03-22 16:39:51 +0100

    updates

--- vault/infra/main.tf
+++ vault/infra/main.tf
@@ -29,7 +29,8 @@ provider "aws" {
 resource "aws_instance" "instance" {
   instance_type = "t3.micro"
   ami = "ami-0110d1b5b1cdd8780"
-  name = data.vault_kv_secret_v2.example.data["creds"]
+  tags = {name = data.vault_kv_secret_v2.example.data["creds"]}
+  
 }
 
 #locals  {  
