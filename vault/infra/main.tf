3c80401c01fa2ecdf4e7e46e71aa8916a1a99f3e
Cedric Feist <cedric.feist@gmail.com>
2023-03-22 14:48:58 +0100

    updates

--- vault/infra/main.tf
+++ vault/infra/main.tf
@@ -1,23 +1,5 @@
 provider "vault" {}
 
-resource "vault_mount" "example" {
-  path    = "example"
-  type    = "kv-v2"
-  options = { version = "2" }
-}
-
-resource "vault_kv_secret_v2" "example" {
-  mount = vault_mount.example.path
-
-  name                = "unsecret"
-  cas                 = 1
-  delete_all_versions = true
-  data_json = jsonencode(
-    {
-      creds = "awscreds"
-    }
-  )
-}
 
 data "vault_kv_secret_v2" "creds" {
   #namespace = "admin"
@@ -26,6 +8,6 @@ data "vault_kv_secret_v2" "creds" {
 
 }
 
-output "secret" {
+output "aws_cred_demo" {
   value = data.vault_kv_secret_v2.creds
 }
