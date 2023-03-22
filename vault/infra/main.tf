641402bc0ce7cf50227b3f97adf12935bd26fd0d
Cedric Feist <cedric.feist@gmail.com>
2023-03-22 14:55:27 +0100

    updates

--- vault/infra/main.tf
+++ vault/infra/main.tf
@@ -1,13 +1,11 @@
 provider "vault" {}
 
-
 data "vault_kv_secret_v2" "creds" {
   #namespace = "admin"
   mount = "example"
   name = "unsecret"
-
 }
 
 output "aws_cred_demo" {
-  value = data.vault_kv_secret_v2.creds
+  value = data.vault_kv_secret_v2.creds.name
 }
