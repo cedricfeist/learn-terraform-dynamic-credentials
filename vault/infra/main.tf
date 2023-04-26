197c10f5980e22e326babc6f2bac4f02741d671c
Cedric Feist <cedric.feist@gmail.com>
2023-04-26 09:46:11 +0200

    instance

--- vault/infra/main.tf
+++ vault/infra/main.tf
@@ -1,12 +1,18 @@
 provider "vault" {}
 
-data "vault_kv_secrets_list_v2" "secrets" {
-  mount = "example"
-}
+#data "vault_kv_secrets_list_v2" "secrets" {
+#  mount = "example"
+#}
 
-data "vault_kv_secret_v2" "creds" {
-  mount = "example"
-  name = "aws"
+#data "vault_kv_secret_v2" "creds" {
+#  mount = "example"
+#  name = "aws"
+#}
+
+data "vault_aws_access_credentials" "awsdynamic" {
+  backend = "aws"
+  role = "aws-role" 
+  
 }
 
 provider "aws" {
@@ -21,11 +27,10 @@ resource "aws_instance" "instance" {
   instance_type = "t2.micro"
   ami = "ami-0110d1b5b1cdd8780"
   count = 1
+  tags = {
+    Name = var.instance_name
+  }
 }
 
-data "vault_aws_access_credentials" "awsdynamic" {
-  backend = "aws"
-  role = "aws-role" 
-  
-}
+
 
